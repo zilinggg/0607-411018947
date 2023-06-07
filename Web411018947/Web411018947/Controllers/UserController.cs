0c2ec1d86aa5dcf8402ad8b7667506a29080c1f2
鐘梓菱 <a25729308@gmail.com>
2023-06-07 19:41:27 +0800

    0607期末

---
+++ Web411018947/Web411018947/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411018947.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music(string name, float? number)
+        
+        {
+            var result = "";
+        
+            ViewBag.Name = name;
+            ViewBag.Number = number;
+            return View();
+        }
+    }
+}
